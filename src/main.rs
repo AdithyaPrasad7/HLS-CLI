ff52b2f81f0b3400c10b2a659ad59aeb16de7e20
Adithya P <prasadadithya714@gmail.com>
2026-03-02 22:24:20 +0530

    feat: Move TOKEN_MGR initialization to main function

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,18 @@ mod config;
 mod token;
 
 use clap::Parser;
+use once_cell::sync::Lazy;
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> { 
   logger::init_logging();
 
   let args = commands::cli::Args::parse();
+
+  pub static TOKEN_MGR: Lazy<Arc<token::TokenManager>> = Lazy::new(|| {
+    Arc::new(token::TokenManager::new("HLS-CLI", "default"))
+});
 
   Ok(())
 }
